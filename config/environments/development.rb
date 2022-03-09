5f130af651798f9106ef67a47bb1cf79ada884d1
Saqib Habib <saqibhabib030@gmail.com>
2022-03-09 14:17:42 +0500

    Third commit

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -75,7 +75,8 @@ Rails.application.configure do
 
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  # config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.default_url_options = { :host => 'my-first-e-commerce-app.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
   address:              'smtp.gmail.com',
@@ -87,6 +88,6 @@ Rails.application.configure do
   enable_starttls_auto: true  }
 
 
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'my-first-e-commerce-app.herokuapp.com'}
 
 end
